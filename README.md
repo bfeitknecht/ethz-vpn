2e0eeca99c24d3c3fa344c1bfc30716ae37a567c
bfeitknecht <bfeitknecht@ethz.ch>
2025-05-31 11:09:46 +0200

    feat: add setup script and improve utility

--- README.md
+++ README.md
@@ -1,39 +1,23 @@
 # ETHZ VPN CLI
 
-This is a small CLI script for managing the ETHZ VPN on macOS. It works but still in progress.
+This is a small `expect` script for managing the ETHZ VPN on macOS. It works but is still in progress.
 
 
 ## Dependencies
-- `totp-cli` database with ETHZ OTP secret token
-- `security` keychain item with VPN password
+
+- `totp-cli` database with ETHZ's secret token for OTP
+- `security` keychain item with VPN and `totp-cli` database password
 
 
 ## Setup
-```
-# install totp-cli and add ETHZ OTP token
-brew install totp-cli
-totp-cli add
-> DB-PASSWD
-> ETH
-> username@ethz.ch
-> TOKEN
-```
 
-
-```
-# add N-ETHZ username and password to Apple Keychain
-security add -s 'eth-vpn' -a 'KÜRZEL' -w 'PASSWORT'
-```
-
+A setup script is provided for convenience. Remember to set `$USERNAME` to your ETHZ username (kürzel) in `ethz-vpn.sh`.
 
 ```
 git clone https://github.com/bfeitknecht/ethz-vpn
-echo "alias vpn=(pwd)/ethz-vpn/ethz-vpn.sh" >> ~/.zshrc
+bash ethz-vpn/setup.sh
+# ...
+echo "alias vpn=$(pwd)/ethz-vpn/ethz-vpn.sh" >> ~/.zshrc
 ```
 
 Or similar for your shell
-
-
-## Development
-- path of vpn binary: `/opt/cisco/secureclient/bin/vpn`
-- get vpn state: `/opt/cisco/secureclient/bin/vpn state | grep -E 'state:' | tail -n 1`
--- ethz-vpn.sh
+++ ethz-vpn.sh
@@ -1,65 +1,70 @@
 #!/usr/bin/env bash
 
-HELP_MESSAGE="figure it out."
+USERNAME=""                 # ETHZ username (kürzel)
+KEY_VPN="ETHZ_VPN"          # keychainItem of ETHZ VPN password
+KEY_TOTP="TOTP_CLI_DB"      # keychainItem of totp-cli database password
+
+# Check if USERNAME is set
+if [ -z "$USERNAME" ]; then
+    echo "*** ERROR: \$USERNAME needs to be set to your ETHZ kürzel!"
+    exit 1
+fi
 
 connect() {
-    expect << 'EOF'
-    #!/usr/bin/expect
+    SCRIPT=$(cat <<-EOF
+		cd /opt/cisco/secureclient/bin
 
-    cd /opt/cisco/secureclient/bin
+		set timeout 3                   ;# timeout in seconds
+		set addr "sslvpn.ethz.ch"       ;# VPN host address
+		set user "$USERNAME@student-net.ethz.ch"        ;# VPN user account
+		set group "1"                                   ;# assuming "student-net" corresponds to group 1
 
-    set timeout 3                   ;# timeout in seconds
-    set addr "sslvpn.ethz.ch"       ;# VPN host address
-    set user "user@student-net.ethz.ch"      ;# ethz username, make this stored in variable?
-    set group "1"                                   ;# assuming "student-net" corresponds to group 1
-    set ETHZ_VPN "ETHZ_VPN"                         ;# keychainItem for vpn account password
-    set TOTP_CLI_DB "TOTP_CLI_DB"                   ;# keychainItem for totp-cli database password
+		# check if VPN is already connected
+		spawn ./vpn state
 
+		expect {
+			"state: Connected" {
+				send_user "\nVPN is already connected. Exiting...\n\n"
+				exit 0
+			}
+			"state: Disconnected" {
+				send_user "\nVPN is not connected. Proceeding...\n\n"
+			}
+			timeout {
+				send_user "\ntimed out while checking VPN state. Exiting...\n\n"
+				exit 1
+			}
+		}
 
+		spawn ./vpn
+		sleep 1
 
-    # check if VPN is already connected
-    spawn ./vpn state
+		expect "VPN>"
+		send -- "connect \$addr\r"
 
-    expect {
-        "state: Connected" {
-            send_user "\nVPN is already connected. Exiting..\n\n"
-            exit 0
-        }
-        "state: Disconnected" {
-            send_user "\nVPN is not connected. Proceeding..\n\n"
-        }
-        timeout {
-            send_user "\ntimed out while checking VPN state. Proceeding..\n\n"
-        }
-    }
+		expect "Group:"
+		send -- "\$group\r"
 
-    spawn ./vpn
-    sleep 1  ;# add a 1-second delay
+		expect "Username:"
+		send -- "\$user\r"
 
-    expect "VPN>"
-    send -- "connect $addr\r"
+		expect "Password:"
+		set password [exec security find-generic-password -s $KEY_VPN -w] ;# retrieve vpn account password from keychain
+		send -- "\$password\r"
 
-    expect "Group:"
-    send -- "$group\r"
+		expect "Second Password:"
+		set otp [exec security find-generic-password -s $KEY_TOTP -w | totp-cli generate ETHZ VPN 2> /dev/null] ;# retrieve TOTP from totp-cli
+		send -- "\$otp\r"
 
-    expect "Username:"
-    send -- "$user\r"
+		expect eof
 
-    expect "Password:"
-    set password [exec security find-generic-password -s $ETHZ_VPN -w] ;# retrieve vpn account password from keychain
-    send -- "$password\r"
+		# expect "state: Connected"
 
-    expect "Second Password:"
-    set TOTP [exec security find-generic-password -s $TOTP_CLI_DB -w | totp-cli generate ETH bfeitknecht@ethz.ch 2>/dev/null] ;# retrieve TOTP from totp-cli
-    send -- "$TOTP\r"
-
-    expect eof
-
-    # expect "state: Connected"
-
-    # uncomment to keep interactive shell open
-    # interact
-    EOF
+		# uncomment to keep interactive shell open
+		# interact
+	EOF
+    )
+    echo "$SCRIPT" | expect -
 }
 
 toggle() {
@@ -71,6 +76,20 @@ toggle() {
 	   connect
 	fi
 }
+
+HELP_MESSAGE="\
+Usage: ethz-vpn.sh <command>
+
+Commands:
+    connect      Connect to the VPN (with automatic 2FA).
+    disconnect   Disconnect from the VPN.
+    toggle       Toggle VPN connection (connect if disconnected, disconnect if connected).
+    status       Show current VPN connection status.
+    stats        Show VPN connection statistics.
+
+Make sure your credentials for the VPN and totp-cli are stored in the macOS Keychain as required.
+"
+
 
 case "$1" in
     toggle)
@@ -89,7 +108,7 @@ case "$1" in
         /opt/cisco/secureclient/bin/vpn status
         ;;
     *)
-        echo $HELP_MESSAGE
+        echo "$HELP_MESSAGE"
         exit 1
         ;;
 esac
